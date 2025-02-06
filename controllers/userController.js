7fdfead0458547a486b0e380781369fe5e81d3f8
Tashika Wijesooriya <it22644244@my.sliit.lk>
2025-02-07 01:52:32 +0530

    add hash and salt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,40 +1,98 @@
+// import User from "../models/user.js";
+// import bcrypt from "bcrypt";
+
+// export function registerUser(req, res) {
+//   const data = req.body;
+
+//   data.password = bcrypt.hashSync(data.password, 10);
+
+//   const newUser = new User(data);
+
+//   newUser
+//     .save()
+//     .then(() => {
+//       res.json({ message: "User added successfully" });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ message: "user could not be added" });
+//     });
+// }
+
+// export function loginUser(req, res) {
+//   const data = req.body;
+
+//   User.findOne({ email: data.email }).then((user) => {
+//     if ((user = null)) {
+//       res.status(404).json({ message: "User not found" });
+//     } else {
+//       const isPasswordCorrectr = bcrypt.compareSync(
+//         data.password,
+//         user.password
+//       );
+
+//       if (isPasswordCorrectr) {
+//         res.json({ message: "Login successful" });
+//       } else {
+//         res.status(401).json({ message: "Login failed" });
+//       }
+//     }
+//   });
+// }
+
+
+
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
-export function registerUser(req, res) {
-  const data = req.body;
+export async function registerUser(req, res) {
+  try {
+    const data = req.body;
 
-  data.password = bcrypt.hashSync(data.password, 10);
+    // Generate a unique salt for this user
+    const salt = bcrypt.genSaltSync(10);
 
-  const newUser = new User(data);
+    // Hash the password using the salt
+    const hashedPassword = bcrypt.hashSync(data.password, salt);
 
-  newUser
-    .save()
-    .then(() => {
-      res.json({ message: "User added successfully" });
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "user could not be added" });
+    // Save both the salt and the hashed password
+    const newUser = new User({
+      ...data,
+      password: hashedPassword,
+      salt: salt, // Store salt in the database
     });
+
+    await newUser.save();
+    res.json({ message: "User added successfully" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "User could not be added", error: err.message });
+  }
 }
 
-export function loginUser(req, res) {
-  const data = req.body;
+export async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
 
-  User.findOne({ email: data.email }).then((user) => {
-    if ((user = null)) {
-      res.status(404).json({ message: "User not found" });
-    } else {
-      const isPasswordCorrectr = bcrypt.compareSync(
-        data.password,
-        user.password
-      );
-
-      if (isPasswordCorrectr) {
-        res.json({ message: "Login successful" });
-      } else {
-        res.status(401).json({ message: "Login failed" });
-      }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  });
+
+    // Retrieve the stored salt (though bcrypt hashes already include it)
+    const { salt, password: hashedPassword } = user;
+
+    // Hash the input password using the stored salt
+    const hashedInputPassword = bcrypt.hashSync(password, salt);
+
+    // Compare the newly hashed password with the stored hashed password
+    if (hashedInputPassword === hashedPassword) {
+      res.json({ message: "Login successful" });
+    } else {
+      res.status(401).json({ message: "Login failed" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", error: err.message });
+  }
 }
+
