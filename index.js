941986b9adde13248378f5b32d4d88ec9d19aa81
Tashika Wijesooriya <it22644244@my.sliit.lk>
2025-02-25 11:45:02 +0530

    Add a product using the user's token.

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
+import jwt from "jsonwebtoken";
 
 let app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  let token = req.headers["authorization"];
+
+  if (token != null) {
+    token = token.replace("Bearer ", "");
+
+    console.log(token);
+
+    jwt.verify(token, "kv-secret-89", (err, decode) => {
+      if (!err) {
+        req.user =decode;
+      }
+    });
+  }
+  next();
+});
 
 let mongoUrl =
   "mongodb+srv://tashika:1234@cluster0.vf5sl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -23,6 +40,6 @@ connection.once("open", () => {
 app.use("/api/users", userRouter)
 app.use("/api/products", productRouter)
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("server is running on port 3000");
 });
