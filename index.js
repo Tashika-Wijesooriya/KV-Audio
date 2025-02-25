111776e313a37e8953100e637a47c67105d3a3eb
Tashika Wijesooriya <it22644244@my.sliit.lk>
2025-02-25 16:41:58 +0530

    create review Router

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import reviewRouter from "./routes/reviewRouter.js";
 dotenv.config();
 
 let app = express();
@@ -40,6 +41,7 @@ connection.once("open", () => {
 
 app.use("/api/users", userRouter)
 app.use("/api/products", productRouter)
+app.use("/api/reviews", reviewRouter)
 
 app.listen(3500, () => {
   console.log("server is running on port 3000");
