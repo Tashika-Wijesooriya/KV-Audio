f6d44ee7ecbd5dd9898780141a2137e128282dbf
Tashika Wijesooriya <it22644244@my.sliit.lk>
2025-03-04 10:43:57 +0530

    change product details

--- models/product.js
+++ models/product.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
+  key: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   name: {
     type: String,
     required: true,
@@ -10,11 +15,25 @@ const productSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  category: {
+    type: String,
+    required: true,
+    default:"uncategorized"
+  },
+  dimension: {
+    type: String,
+    required: true,
+  },
 
   description: {
     type: String,
     required: true,
   },
+  availability: {
+    type: Boolean,
+    required: true,
+    default: true,
+  }
 });
 
 const Product = mongoose.model("Product", productSchema);
