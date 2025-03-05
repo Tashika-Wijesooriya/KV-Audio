e99ff36c4b571c6185ac4981ad0f3f4f4c98682d
Tashika Wijesooriya <it22644244@my.sliit.lk>
2025-03-05 09:57:47 +0530

    create inquiry js

---
+++ models/inquiry.js
@@ -0,0 +1,39 @@
+import { response } from "express";
+import mongoose from "mongoose";
+
+const inquirySchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+    },
+  
+    email: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+    response: {
+        type: String,
+        required: true,
+        default:""
+    },
+    isResolved: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+});
+const Inquiry = mongoose.model("inquiry", inquirySchema);
+
+export default Inquiry;
